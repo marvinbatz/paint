ba7e336e2b0270f5bfc9f6ac7d9bc20dd0af6d08
marvinbatz <marvinbatzz@gmail.com>
2022-05-15 01:35:03 -0600

    Agregando seleccion de color

--- eventos.js
+++ eventos.js
@@ -1,12 +1,22 @@
 var movimiento;
+var colorSeleccionado;
 
 // Escuchamos la techa que se oprime -- keydown cuando se oprime, keyup cuando se deja de oprimir
 var cuadro = document.getElementById("paint_area");
+var color = document.getElementById("color_pincel");
 var paper = cuadro.getContext("2d");
 
 cuadro.addEventListener("mousedown", dibujarMouse);
 cuadro.addEventListener("mousemove", dibujo)
 cuadro.addEventListener("mouseup", dejarDibujarMouse);
+
+color.addEventListener("mousedown", colorPincel);
+
+function colorPincel(evento)
+{
+    colorSeleccionado = color.value;
+    console.log(colorSeleccionado);
+}
 
 // Paso lienzo como parametro
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
@@ -27,10 +37,10 @@ function dibujarMouse(evento)
 
 function dibujo(evento)
 {
+    colorPincel();
     if (movimiento == 1)
     {
-        colorLinea = "blue";
-        dibujarLinea(colorLinea, evento.clientX-1, evento.clientY-1, evento.clientX+1, evento.clientY+1, paper);
+        dibujarLinea(colorSeleccionado, evento.clientX-1, evento.clientY-1, evento.clientX+1, evento.clientY+1, paper);
     }
 }
 
